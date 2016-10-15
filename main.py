4c762bd2941095d0fb649c80f4e4c33e104a1e3a
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-10-15 15:43:08 -0700

    rev. 1

--- main.py
+++ main.py
@@ -23,50 +23,69 @@ def raids():
 	return random.choice(raids)
 
 def legend():
-	legend = ["Bone Head 2.0"
-		"Saturn"
-		"DJ Hunter Hellquist"
-		"Rat Brothers"
-		"BNKR"
-		"Badass Creepers"
-		"Blue"
-		"Black Queen"
-		"Jimbo & Tector Hodunk"
-		"McNally"
-		"Mick Zaford"
-		"Mobley"
-		"Gettle"
-		"Wilhelm"
-		"King Mong"
-		"Laney White"
-		"Rakkman"
-		"Smash Head"
-		"Shorty"
-		"Clayton"
-		"Spycho"
-		"Scorch"
-		"Old Slappy"
-		"Mad Dog"
-		"Dukino's Mom"
-		"Sherrif Of Lynchwood"
-		"Foreman"
-		"Fatty Enemies"
-		"Mortar"
-		"Boom & Bewm"
-		"Captain Flynt"
-		"Midgemong"
-		"The Four Assassins"
-		"Boll"
-		"Savage Lee"
-		"Doc Mercy"
-		"Madame Von Bartlesby"
-		"The Warrior"
-		"Pimon"
-		"Son of Mothrakk"
-		"Tumba"
-		"Loot Midgets"
+	legend = ["Bone Head 2.0",
+		"Saturn",
+		"DJ Hunter Hellquist",
+		"Rat Brothers",
+		"BNKR",
+		"Badass Creepers",
+		"Blue",
+		"Black Queen",
+		"Jimbo & Tector Hodunk",
+		"McNally",
+		"Mick Zaford",
+		"Mobley",
+		"Gettle",
+		"Wilhelm",
+		"King Mong",
+		"Laney White",
+		"Rakkman",
+		"Smash Head",
+		"Shorty",
+		"Clayton",
+		"Spycho",
+		"Scorch",
+		"Old Slappy",
+		"Mad Dog",
+		"Dukino's Mom",
+		"Sherrif Of Lynchwood",
+		"Foreman",
+		"Fatty Enemies",
+		"Mortar",
+		"Boom & Bewm",
+		"Captain Flynt",
+		"Midgemong",
+		"The Four Assassins",
+		"Boll",
+		"Savage Lee",
+		"Doc Mercy",
+		"Madame Von Bartlesby",
+		"The Warrior",
+		"Pimon",
+		"Son of Mothrakk",
+		"Tumba",
+		"Loot Midgets",
 		"Knuckle Dragger"]
 	return(random.choice(legend))
+
+while True:
+	print("Press Return to Randomize")
+	print(" ")
+	print("Seraph Boss")
+	print(seraphs())
+
+	print(" ")
+	print("Raid Boss")
+	print(raids())
+
+	print(" ")
+	print("Re-killable Enemy")
+	print(legend())
+
+	print("-------------------------")
+
+	input()
+
 
 
 
