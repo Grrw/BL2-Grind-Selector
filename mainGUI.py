8f73379ae66b76a3f3c55640813aeba4fe3c5f28
Grrw <GitYBenY-a4h1@mailnicks.org>
2016-11-22 11:11:49 -0800

    add 'all' option

--- mainGUI.py
+++ mainGUI.py
@@ -15,27 +15,35 @@ easygui.msgbox("""
 mExit = False
 while not mExit:
 
-	mQuit = easygui.buttonbox('Select an Enemy Type', 'Main Menu', ('Seraphs', 'Raid Bosses', 'Legendaries', 'Quit'))
+
+	mQuit = easygui.buttonbox('Select an Enemy Type', 'Main Menu', ('Seraphs', 'Raid Bosses', 'Legendaries', 'All', 'Quit'))
 	if mQuit == 'Quit':
 		mExit = True
-	elif mQuit == 'Seraphs':
+	elif mQuit == 'Seraphs': # Pick a Seraph
 		sExit = False
 		while not sExit:
 			sQuit = easygui.buttonbox(enemies.seraphs(), 'Seraphs', ('Reroll', 'Back'))
 			if sQuit == 'Back':
 				sExit = True
-	elif mQuit == 'Raid Bosses':
+	elif mQuit == 'Raid Bosses': # Pick a Raid
 		rExit = False
 		while not rExit:
 			rQuit = easygui.buttonbox(enemies.raids(), 'Raid Bosses', ('Reroll', 'Back'))
 			if rQuit == 'Back':
 				rExit = True
-	elif mQuit == 'Legendaries':
+	elif mQuit == 'Legendaries': # Pick all others
 		lExit = False
 		while not lExit:
 			lQuit = easygui.buttonbox(enemies.legend(), 'Legendaries', ('Reroll', 'Back'))
 			if lQuit == 'Back':
 				lExit = True
+	elif mQuit == 'All': # Pick all others
+		aExit = False
+		while not aExit:
+			aQuit = easygui.buttonbox(enemies.all(), 'All', ('Reroll','Back'))
+			if aQuit == 'Back':
+				aExit = True
+
 	else:
 		print('You shouldn\'t get here')
 			
